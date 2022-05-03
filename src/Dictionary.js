041c69dd7695c4f8639eafd6a22c16aa748d1568
E-C-Shackelford <98488905+E-C-Shackelford@users.noreply.github.com>
2022-05-03 13:21:53 -0700

    comment out steps not needed anymore

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,14 +8,14 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    //console.log(response.data[0]);
     // console.log(response.data[0].meanings[0].definitions[0].definition);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for the definition of ${keyword}`);
+    //alert(`Searching for the definition of ${keyword}`);
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
